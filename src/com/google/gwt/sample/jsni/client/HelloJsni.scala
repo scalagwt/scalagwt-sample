1b8a5089a51724b7de3956ac960c0af3afec6573
Matthew Neeley <mneeley@gmail.com>
2011-10-28 04:04:57 -0400

    added some further demos of jsni functionality

--- src/com/google/gwt/sample/jsni/client/HelloJsni.scala
+++ src/com/google/gwt/sample/jsni/client/HelloJsni.scala
@@ -1,31 +1,64 @@
-package com.google.gwt.sample.jsni.client;
+package com.google.gwt.sample.jsni.client
 
-//import scala.util.nativeCode;
+//import scala.util.nativeCode
 
-import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.JsArrayInteger;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.core.client.EntryPoint
+import com.google.gwt.core.client.JavaScriptObject
+import com.google.gwt.core.client.JsArrayInteger
+import com.google.gwt.event.dom.client.ClickEvent
+import com.google.gwt.event.dom.client.ClickHandler
+import com.google.gwt.user.client.ui.Button
+import com.google.gwt.user.client.Window
+import com.google.gwt.user.client.ui.RootPanel
+
+object HelloJsni {
+  @native def getMessage(x: Int, y: Int): Point = nativeCode { """
+    return [x, y];
+  """ }
+}
 
 class HelloJsni extends EntryPoint {
-
+  
   def onModuleLoad() {
-    val numbers = getList
+    val numbers = getList(10)
     val s = numbers join ", "
-    val b = new Button("Click me", (_: ClickEvent) => Window.alert("Hello, AJAX, said Scala\nThose numbers are coming from jsni!\n" + s));
-    RootPanel.get().add(b);
+    val p = getMessage(1, 2)
+    val p2 = HelloJsni.getMessage(3, 4)
+    //val p3 = Point.create(5, 6) // doesn't work in hosted mode
+    val b = new Button("Click me", (_: ClickEvent) => Window.alert("Hello, AJAX, said Scala\nThose numbers are coming from jsni!\n" + s))
+    val b2 = new Button("Click me", (_: ClickEvent) => Window.alert("Point: x=" + p.x + ", y=" + p.y))
+    val b3 = new Button("Click me", (_: ClickEvent) => Window.alert("Point: x=" + p2.x + ", y=" + p2.y))
+    //val b4 = new Button("Click me", (_: ClickEvent) => Window.alert("Point: x=" + p3.x + ", y=" + p3.y))
+    RootPanel.get().add(b)
+    RootPanel.get().add(b2)
+    RootPanel.get().add(b3)
+    //RootPanel.get().add(b4)
   }
   
   implicit def clickHandler(f: ClickEvent => Unit): ClickHandler = new ClickHandler {
     def onClick(event: ClickEvent) = f(event)
   }
   
-  @native def nativeCode(s: String): Nothing = sys.error("nativeCode implementation missing!!!")
-  
-  def getList: JsArrayInteger = nativeCode { """
-    return [1, 2, 3, 4];
+  @native def getMessage(x: Int, y: Int): Point = nativeCode { """
+    return [x, y];
   """ }
+  
+  @native def getList(len: Int): JsArrayInteger = nativeCode { """
+    var arr = [];
+    for (var i = 0; i < len; i++) {
+      arr.push(i);
+    }
+    return arr;
+  """ }
+}
+
+object Point {
+  @native def create(x: Int, y: Int): Point = nativeCode { """
+    return [x, y];
+  """ }
+}
+
+class Point protected() extends JavaScriptObject {
+  @native def x: Int = nativeCode("return this[0];")
+  @native def y: Int = nativeCode("return this[1];")
 }
---
+++ src/com/google/gwt/sample/jsni/client/package.scala
@@ -0,0 +1,5 @@
+package com.google.gwt.sample.jsni
+
+package object client {
+  def nativeCode(s: String): Nothing = sys.error("nativeCode implementation missing")
+}
